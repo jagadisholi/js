0a244f96da62498f53548b7a727f7ad60c296d69
jagadisholi <olijagadish1@gamil.com>
2025-10-15 14:06:23 +0545

    day5

---
+++ day5/app.js
@@ -0,0 +1,77 @@
+// array in js 
+let a = 10;
+let b = 10 ; 
+let c = 10; 
+
+let num = [10, 10 , 10];
+console.log(a,b,c, num);
+
+const aa =[];
+console.log(aa);
+
+
+let aaa = new Array(10,10,10);
+console.log(aaa);
+
+let number = [1,2,3,4];
+console.log(number[0]);
+console.log(number[2]);
+
+let first =number[0];
+console.log("first item:", first);
+
+let last = number[number.length-1];
+console.log("last element:", last);
+
+console.log(number);
+
+// modify
+number[2] = 6;
+console.log(number);
+
+// adding element on array
+number.push(7);
+console.log(number);
+// adding element to the begining 
+number.unshift(8);
+console.log(number);
+// removing 
+number.pop();
+console.log(number);
+// remove first element 
+number.shift();
+console.log(number);
+// remove 2 element starting from index 1
+number.splice(1,2);
+console.log(number);
+// array length 
+let len = number.length;
+console.log("array length : "+len);
+//increase and decrease 
+number.length = 7;
+console.log(number);
+
+number.length = 3;
+console.log(number);
+
+// array concatination 
+let concatarray = number.concat(num);
+console.log(concatarray);
+
+// type of array
+console.log(typeof number);
+
+// js array delete operator 
+let emp = {
+    first : "hari",
+    second: "ram",
+    third: "sita"
+}
+console.log(delete emp.third);
+console.log(emp);
+
+// js array flat( ) method 
+const a1 = [[1,2],[3,4,5],[6,7], [8,9,0,1]];
+const a2 = a1.flat(Infinity);
+console.log(a2);
+
