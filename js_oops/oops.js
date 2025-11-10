6353d89e26d3508ba0517d8915811e5360839920
jagadisholi <olijagadish1@gamil.com>
2025-11-10 20:32:55 +0545

    class

---
+++ js_oops/oops.js
@@ -0,0 +1,25 @@
+class dog{
+    constructor(name){
+        this.name =name;
+    }
+    get dogname(){ // getter method
+        return this.name;    
+    }
+    //setter method
+    set dogname(newname){
+        this.name =newname;
+    }
+    //regular method
+    dog__regular_method(){
+        console.log(`${this.name} says woof!`);
+        
+    }
+}
+
+let mydog = new dog("abc");  // using getter method
+console.log(mydog.name);
+
+mydog.dogname = 'abb' //using setter method
+console.log(mydog.name);
+
+mydog.dog__regular_method();
